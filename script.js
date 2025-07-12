cd77c2e293a97a99acd03ee9b32df2b34d5fb7a1
gadregath <gadregath@gmail.com>
2025-07-12 19:30:08 -0300

    Add function to generate computer choice

---
+++ script.js
@@ -0,0 +1,13 @@
+function getComputerChoice() {
+    return Math.floor(Math.random() * 3);
+}
+
+let computerChoice = getComputerChoice();
+
+if (computerChoice === 0) {
+    console.log('Rock');
+} else if (computerChoice === 1) {
+    console.log('Paper');
+} else if (computerChoice === 2) {
+    console.log('Scissors');
+}
