fe3cf6dd9d305fbec8a86ae75774ab3f065900a3
gadregath <gadregath@gmail.com>
2025-07-12 20:37:49 -0300

    Add user input

--- script.js
+++ script.js
@@ -11,3 +11,17 @@ if (computerChoice === 0) {
 } else if (computerChoice === 2) {
     console.log('Scissors');
 }
+
+function getHumanChoice(){
+    return prompt('Make your move');
+}
+
+let humanChoice = getHumanChoice();
+
+if (humanChoice === 'Rock') {
+    console.log('Rock');
+} else if (humanChoice === 'Paper') {
+    console.log('Paper');
+} else if (humanChoice === 'Scissors') {
+    console.log('Scissors');
+}
